ecd714ddb929903c9a65408897c13cb930139375
ricksonmenezes <ricksonmenezes@gmail.com>
2024-06-01 12:19:25 +0800

    facade pattern to abstract the application start and stop by passing client server as an object into the facade

--- src/test/kotlin/SeeATodoListAT.kt
+++ src/test/kotlin/SeeATodoListAT.kt
@@ -5,9 +5,8 @@ import com.rgarage.domain.ToDoItem
 import com.rgarage.domain.ToDoList
 import com.rgarage.domain.User
 import org.http4k.client.JettyClient
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Status
+import org.http4k.core.*
+import org.http4k.filter.ClientFilters
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.junit.jupiter.api.Test
@@ -20,36 +19,38 @@ import strikt.assertions.isEqualTo
 
 class SeeATodoListAT {
 
+    val frank = ToDoListOwner("Frank")
+    val shoppingItems = listOf("carrots", "apples", "milk")
+    val frankList = frank.createList("shopping", shoppingItems)
+    val bob = ToDoListOwner("Bob")
+    val gardenItems = listOf("fix the fence", "mowing the lawn")
+    val bobList =  bob.createList("gardening", gardenItems)
+    val lists = mapOf(
+        frank.asUser() to listOf(frankList),
+        bob.asUser() to listOf(bobList)
+    )
+
+    fun ToDoListOwner.asUser() : User = User(name)
+
     @Test
     fun `List owners can see their lists`() {
 
-        val listName = "shopping"
-        val foodToBuy = listOf("carrots", "apples", "milk")
-
-
-        val frank = ToDoListOwner("frank")
-
-        /*when we start the app, we are sending the list into the  app start class Zettai*/
-        startTheApplication(frank.name, listName, foodToBuy)
-        val list = frank.getToDoList(frank.name, listName)
-        //val list = getToDoList(userName, listName)
-
-        expectThat(list.listName.name).isEqualTo(listName)
-        expectThat(list.items.map { it.description }).isEqualTo(foodToBuy)
+        val app = startTheApplication(lists);
+        app.runScenario {
+            frank.canSeeTheList("shopping", shoppingItems, it)
+            bob.canSeeTheList("gardening", gardenItems, it)
+        }
     }
+
 
     @Test
     fun `Only owners can see their lists`() {
-        val listName = "shopping"
-        val foodToBuy = listOf("carrots", "apples", "milk")
-        val frank = ToDoListOwner("frank")
-        startTheApplication(frank.name,listName, foodToBuy)
-        frank.canSeeThelist(listName, foodToBuy)
+        val app = startTheApplication(lists);
+        app.runScenario {
+            frank.canNotSeeTheList("gardening", shoppingItems, it)
+            bob.canNotSeeTheList("shopping", gardenItems, it)
+        }
     }
-
-
-
-
 
     interface ScenarioActor{ val name: String
     }
@@ -64,18 +65,31 @@ class SeeATodoListAT {
             start(frank.name, createList(listName, foodToBuy))
 
             }*/
-        fun canSeeThelist(listName: String, items: List<String>) {
+        fun canSeeTheList(listName: String, items: List<String>, app: ApplicationForAT) {
 
             val expectedList = createList(listName, items);
-            val list = getToDoList(name, listName )
+            val list = app.getToDoList(name, listName )
             expectThat(list).isEqualTo(expectedList)
 
         }
 
-        private fun createList(listname: String, items: List<String>) =
+        fun canNotSeeTheList(listName: String, items: List<String>, app: ApplicationForAT) {
+
+            expectThrows<AssertionFailedError> {
+                app.getToDoList(name, listName)
+            }
+
+        }
+
+        fun createList(listname: String, items: List<String>) =
             ToDoList(ListName(listname), items.map ( ::ToDoItem ))
 
-         fun getToDoList(user: String, listName: String): ToDoList {
+
+    }
+
+    class ApplicationForAT(val client: HttpHandler, val server: AutoCloseable) {
+
+        fun getToDoList(user: String, listName: String): ToDoList {
             val client = JettyClient()
             val response = client(Request(Method.GET, "http://localhost:8081/todo/$user/$listName"))
 
@@ -99,22 +113,27 @@ class SeeATodoListAT {
 
         private fun extractItemDesc(matchResult: MatchResult): String = matchResult.value.substringAfter("<td>").dropLast(1)
 
+        fun runScenario(steps: (ApplicationForAT)->Unit) { server.use {
+            steps(this) }
+        }
+    }
+
     }
 
     private fun startTheApplication(
-        user: String,
-        listName: String,
-        items: List<String>
-    ) {
-        val toDoList = ToDoList(
-            ListName(listName),
-            items.map(::ToDoItem)
-        )
-        val lists = mapOf(User(user) to listOf(toDoList))
+        lists : Map<User, List<ToDoList>>
+    ) : SeeATodoListAT.ApplicationForAT {
+
+        val port = 8081
+
         /*when we start the app, we are sending the list into the  app start class Zettai
         * But Zettai has no variable to store the lists. It's  probably implicitly stored. */
         val server = Zettai(lists).asServer(Jetty(8081)) //different from main
         server.start()
+
+        val client = ClientFilters .SetBaseUriFrom(Uri.of("http://localhost:$port/")) .then(JettyClient())
+        return SeeATodoListAT.ApplicationForAT(client, server)
+
     }
 
 }
